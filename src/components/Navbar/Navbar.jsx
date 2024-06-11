703c0d36debc14ee5999024c428770876c9b26c4
Sarthak Mittal <sarmittal@gmail.com>
2024-06-11 22:45:19 +0530

    cart page added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({setShowLogin}) => {
   const[menu, setMenu] = useState("menu");
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo'/>
+      <Link to='/'><img src={assets.logo} alt="" className='logo'/></Link>
       <ul className='navbar-menu'>
         <Link onClick={()=>setMenu("home")}className={menu==="home"?"active":""}>home</Link>
         <a href='#exploremenu' onClick={()=>setMenu("menu")}className={menu==="menu"?"active":""}>menu</a>
@@ -15,7 +15,7 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
           <div className="dot"></div>
         </div>
         <button onClick={()=>{
--- src/context/storeContext.jsx
+++ src/context/storeContext.jsx
@@ -1,43 +1,56 @@
 import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
-const StoreContextProvider = (props) =>{
-    const [cartItems, setCartItems] = useState({})
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
-    const addToCart =(itemId)=>{
-        if (!cartItems[itemId]) {
-            setCartItems((prev)=>({
-                ...prev,
-                [itemId]:1
-            }))
-        }else{
-            setCartItems((prev)=>({
-            ...prev,
-            [itemId]:prev[itemId]+1
-        }))
-
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: 1,
+      }));
+    } else {
+      setCartItems((prev) => ({
+        ...prev,
+        [itemId]: prev[itemId] + 1,
+      }));
+    }
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: prev[itemId] - 1,
+    }));
+  };
+  const getTotalCartAmount = () => {
+    let totalamount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let iteminfo = food_list.find((product) => {
+          return product._id === item;
+        });
+        if(iteminfo){
+            totalamount += iteminfo.price * cartItems[item];
         }
+      }
     }
-    const removeFromCart = (itemId)=>{
-        setCartItems((prev)=>({
-            ...prev,
-            [itemId]:prev[itemId]-1
-        }))
-    }
-    const contextValue = {
-        food_list,
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart
-    }
+    return totalamount;
+  };
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+    getTotalCartAmount
+  };
 
-    return(
-        <StoreContext.Provider value = {contextValue}>
-            {props.children}
-        </StoreContext.Provider>
-
-    )
-}
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
 export default StoreContextProvider;
